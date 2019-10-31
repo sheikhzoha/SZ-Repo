2b734ca8939310b07bd60dee608c69e80b11e92f
sheikhzoha <sheikhzohais@gmail.com>
2019-11-01 00:10:19 +0530

    create

---
+++ sz-company/src/main/java/com/sheikh/szcompany/SzCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.sheikh.szcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzCompanyApplication.class, args);
+	}
+
+}
---
+++ sz-company/src/test/java/com/sheikh/szcompany/SzCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheikh.szcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
