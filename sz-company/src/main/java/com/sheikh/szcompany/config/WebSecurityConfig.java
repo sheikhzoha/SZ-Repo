c54265e15a92759177786e930658985c2e245bf1
Sheikh Zuha <sheikh.zuha@sap.com>
2019-11-08 16:42:36 +0530

    authorization

--- sz-company/src/main/java/com/sheikh/szcompany/config/WebSecurityConfig.java
+++ sz-company/src/main/java/com/sheikh/szcompany/config/WebSecurityConfig.java
@@ -1,25 +1,35 @@
 package com.sheikh.szcompany.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// TODO Auto-generated method stub
-		auth.inMemoryAuthentication().withUser("admin").password("password");
+		auth.inMemoryAuthentication().withUser("admin").password("password").roles("ADMIN");
 
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		//http.authorizeRequests().
-		super.configure(http);
+	
+	  @Override 
+	  protected void configure(HttpSecurity http) throws Exception { //
+		  //TODO Auto-generated method stub 
+		  http.authorizeRequests().
+		  antMatchers("/","/home").permitAll().anyRequest().authenticated().and().httpBasic();
+		  //super.configure(http); 
+	  }
+	 
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
-	
-	
 
 }
